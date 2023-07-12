a8ec672c63d6f26115859f3ab5e75e92dbcef4c1
Natig Kurbanov <nkurbanov6@gmail.com>
2023-07-12 23:28:32 +0400

    added comment

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -1,11 +1,17 @@
-import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {};
+// Tests won't work without firebaseConfig, so do not even try
+const firebaseConfig = {
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-getAnalytics(app);
+const analytics = getAnalytics(app);
+
 export default firebaseConfig;
